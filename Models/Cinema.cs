d3e702cfb430b5e5c249ccf5a1bf0cdd4402beea
Jeevan <53844739+G1Grg@users.noreply.github.com>
2025-02-02 22:12:43 +1000

    Add project files.

---
+++ Models/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Cinema
+    {
+        [Key] 
+        public int Id { get; set; }
+        public string? Logo { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        // Realtionships: 1 Cinema can produce multiple movies
+        // That is: one to Many realtionship from Cinema to Movie
+        public List<Movie>? Movies { get; set; }
+
+    }
+}
---
+++ Models/Producer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? ProfilePictureURL { get; set; }
+        public string? FullName { get; set; }
+        public string? Bio { get; set; }
+
+        // Realtionships: 1 Producer can produce multiple movies
+        // That is: one to Many realtionship from Producer to Movie
+
+        public List<Movie>? Movies { get; set; }
+    }
+}
