c72c42d0d30a225509b9a1362d16b8c1e1419db4
Jeevan <53844739+G1Grg@users.noreply.github.com>
2025-02-04 17:20:39 +1000

    Creating Actors, Cinemas, Movies, and Producers Controller. These controllers are responsible for different CRUD functions that are accesible for each contents.

---
+++ Controllers/Producers.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class Producers : Controller
+    {   
+        public readonly AppDbContext _context;
+
+        public Producers(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
