1363727cdbaadf01b7f2fc9fc674f9d2849505a8
Jeevan <53844739+G1Grg@users.noreply.github.com>
2025-03-03 20:23:12 +1000

    Major Update:  --> Listing Dropdown Lists from database

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Controllers
@@ -30,10 +31,17 @@ namespace eTickets.Controllers
         }
 
 
-        //GEt: Movies/Create
+        //GET: Movies/Create
+        public async Task<IActionResult> Create()
+        {
+            var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
 
-        public IActionResult Create() {
+            ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+            ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
             return View();
         }
+
     }
 }
